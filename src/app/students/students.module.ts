94547aadbe0e670b3c85903a63b5fb880a4cd292
liadber <liadbercovich@gmail.com>
2021-12-15 17:41:43 +0200

    Optional - Lazy Loading - app-routing.module.ts

---
+++ src/app/students/students.module.ts
@@ -0,0 +1,15 @@
+// (Optional - Modules && Lazy Loading)
+
+import {NgModule} from '@angular/core';
+import {StudentsComponent} from './students.component';
+
+@NgModule({
+  declarations: [
+    StudentsComponent,
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class StudentsModule { }
