0af2fcbcf295dc6694fbbfe36c620330f4beee31
liadber <liadbercovich@gmail.com>
2021-12-15 16:09:21 +0200

    Initial Commit

---
+++ src/app/common/student.model.ts
@@ -0,0 +1,5 @@
+export interface Student {
+  entityId: number;
+  field1: number;
+  field2: string;
+}
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-module',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
