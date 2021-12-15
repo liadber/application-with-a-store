6bf99dd53a88879dcdb8ed4be545657d68cc7611
liadber <liadbercovich@gmail.com>
2021-12-15 16:10:13 +0200

    Define State

---
+++ src/app/store/school-app-state.ts
@@ -0,0 +1,5 @@
+export const SCHOOL_APP_BASIC_STATE_KEY = 'School App Basic State';
+
+export interface SchoolAppState {
+  [SCHOOL_APP_BASIC_STATE_KEY]: null;
+}
---
+++ src/app/students/store/students.state.ts
@@ -0,0 +1,34 @@
+import {Student} from '../../common/student.model';
+import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
+import * as fromSchoolApp from '../../store/school-app-state';
+
+export const STUDENTS_KEY = 'students';
+
+export interface State extends EntityState<Student> {
+  // additional state properties
+  selectedStudentId: number | null;
+  averageGrade: number;
+}
+
+/* functions for the EntityAdapter */
+export function sortByName(a: Student, b: Student): number {
+  return a.entityId - b.entityId;
+}
+
+export const adapter: EntityAdapter<Student> = createEntityAdapter<Student>({
+  selectId: a => a.entityId,
+  sortComparer: sortByName, // (optional)
+});
+
+
+export interface StudentsState extends fromSchoolApp.SchoolAppState {
+  // (Optional - FeatureModule, add the module state to the root state.
+  // Note: using [] in interface definition ables to using variable name.
+  [STUDENTS_KEY]: State;
+}
+
+export const simulationsInitialState: State = adapter.getInitialState({
+  entities: /*demo-data*/ [{entityId: 1, field1: 1, field2: 'a'}, {entityId: 1, field1: 1, field2: 'a'}],
+  selectedStudentId: 1,
+  averageGrade: 82
+});
